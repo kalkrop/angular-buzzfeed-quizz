2851ce18d38646b45832542821f4577ce0454b1c
Anderson de Araújo Alves <anderson.araujoa@gmail.com>
2024-02-26 15:34:10 -0400

    Criando a estrutura base do projeto

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { QuizzComponent } from "../../components/quizz/quizz.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  imports: [QuizzComponent]
 })
 export class HomeComponent {
 
